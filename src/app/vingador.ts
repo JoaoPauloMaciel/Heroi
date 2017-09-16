4f4539303ddb6aa40df032807d0d6946989e81f0
Linguagem de Prog. para Web <lpweb.pmw@labin.canoas>
2017-09-16 15:50:51 -0300

    Heroi2

---
+++ src/app/vingador.ts
@@ -0,0 +1,13 @@
+export class Vingador {
+    id: number;
+    nome: string;
+    pessoa: string;
+    urlVingador: string;
+
+    constructor(id: number, nome: string, pessoa: string, urlVingador:string ) {
+        this.id = id;
+        this.nome = nome;
+        this.pessoa = pessoa;
+        this.urlVingador = urlVingador;
+    }
+}
